b5b14d534e63b37ce35b3e0e5bdfc4ba7e70bf93
Sushin K <sushink70@gmail.com>
2025-02-09 10:05:12 +0530

    new files added

--- fillone.h
+++
@@ -1,4 +0,0 @@
-//Fill the memory sectors with 1
-int fill_sectors_with_one(){
-    return 0;
-}
--- fillzero.h
+++
@@ -1,8 +0,0 @@
-#include <iostream>
-
-using namespace std;
-
-int fill_sectors_with_zero(){
-    return 0;
-}
-
